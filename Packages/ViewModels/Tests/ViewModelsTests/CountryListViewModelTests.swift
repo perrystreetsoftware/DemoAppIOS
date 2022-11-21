0c64477526bf3894f9aa935e9a6cc95775b380c5
Matheus Dutra <matheusd@perrystreet.com>
2022-11-21 11:05:33 -0300

    Fix unit test

--- Packages/ViewModels/Tests/ViewModelsTests/CountryListViewModelTests.swift
+++ Packages/ViewModels/Tests/ViewModelsTests/CountryListViewModelTests.swift
@@ -45,14 +45,14 @@ final class CountryListViewModelTests: QuickSpec {
                 stateRecorder = viewModel.$state.record()
             }
 
-            var states: [CountryListViewModel.UiState]!
-
             it("then it startings having transitioned to .loading") {
                 expect(try! stateRecorder.availableElements.get()).to(equal([
                     CountryListViewModel.UiState(isLoading: true, serverStatus: .Empty)]))
             }
 
-            context("when I advance") {
+            context("when a country is returned") {
+                var states: [CountryListViewModel.UiState]!
+
                 beforeEach {
                     countryToBeReturned.send(.init())
                     countryToBeReturned.send(completion: .finished)
@@ -60,13 +60,11 @@ final class CountryListViewModelTests: QuickSpec {
                     states = try! stateRecorder.availableElements.get()
                 }
 
-                it("then the middle emission is still loading") {
+                it("then we should emit a loading and a loaded state") {
                     expect(states[1].isLoading).to(beTrue())
                     expect(states[1].isLoaded).to(beFalse())
                     expect(states[1].continents.isEmpty).to(beFalse())
-                }
-
-                fit("then the final emission has loaded") {
+                    
                     expect(states[2].isLoading).to(beFalse())
                     expect(states[2].isLoaded).to(beTrue())
                     expect(states[2].continents.isEmpty).to(beFalse())
