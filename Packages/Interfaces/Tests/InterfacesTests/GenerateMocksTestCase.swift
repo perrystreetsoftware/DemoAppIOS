eeb72458f3c910ed1bf0545a3021bdfd1fe04e40
Matheus Dutra <matheusd@perrystreet.com>
2022-11-21 15:39:18 -0300

    Add comment

--- Packages/Interfaces/Tests/InterfacesTests/GenerateMocksTestCase.swift
+++ Packages/Interfaces/Tests/InterfacesTests/GenerateMocksTestCase.swift
@@ -11,6 +11,10 @@ import Mockingbird
 import InterfacesMocks
 
 class GenerateMocksTestCase: XCTestCase {
+    
+    /// This unit test is responsible for generating the package mocks. All interfaces declared here will have a generated mock on Mocks.generated file
+    /// Run `./gen-mocks.sh` to generate these mocks
+    
     func test_generate_mockingbird_mocks() {
         _ = mock(TravelAdvisoryApiImplementing.self)
     }
