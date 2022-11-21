c15e1f0b089b449b0faf6c4bd7461c3dae75ab19
Matheus Dutra <matheusd@perrystreet.com>
2022-11-21 10:39:14 -0300

    Fix repository tests

--- Packages/Repositories/Tests/RepositoriesTests/CountryListRepositoryTests.swift
+++ Packages/Repositories/Tests/RepositoriesTests/CountryListRepositoryTests.swift
@@ -1,107 +1,103 @@
-////
-////  File.swift
-////
-////
-////  Created by Eric Silverberg on Sep 18, 2022
-////
 //
-//import Quick
-//import Nimble
-//import Swinject
-//import Foundation
-//import DomainModels
-//import CombineExpectations
-//import Interfaces
-//import Combine
-//import Repositories
+//  File.swift
 //
-//final class CountryListRepositoryTests: QuickSpec {
-//    override func spec() {
-//        describe("CountryListRepository") {
-//            var container: Container!
-//            var repository: CountryListRepository!
-//            var mockAppScheduler: MockAppSchedulerProviding!
-//            var api: MockTravelAdvisoryApi!
-//            var continentsRecorder: Recorder<[Continent], Never>!
-//            var continents: [Continent]!
 //
-//            beforeEach {
-//                container = Container().injectBusinessLogicRepositories()
-//                    .injectInterfaceLocalMocks()
-//                    .injectInterfaceRemoteMocks()
-//                mockAppScheduler = (container.resolve(AppSchedulerProviding.self)! as! MockAppSchedulerProviding)
-//                mockAppScheduler.useTestMainScheduler = true
-//                repository = container.resolve(CountryListRepository.self)!
-//                api = (container.resolve(TravelAdvisoryApiImplementing.self)! as! MockTravelAdvisoryApi)
+//  Created by Eric Silverberg on Sep 18, 2022
 //
-//                continentsRecorder = repository.$continents.record()
-//                continents = try! QuickSpec.current.wait(for: continentsRecorder.next(), timeout: 5.0)
-//            }
-//
-//            it("then continents starts empty") {
-//                expect(continents.count).to(equal(0))
-//            }
-//
-//            describe("#reload") {
-//                var recorder: Recorder<Void, CountryListError>!
-//                var completion: Subscribers.Completion<CountryListError>!
-//                var apiResult: Result<CountryListDTO, TravelAdvisoryApiError>?
-//
-//                beforeEach {
-//                    api.getCountryListResult = apiResult
-//                    recorder = repository.reload().record()
-//                    mockAppScheduler.testScheduler.advance()
-//                }
-//
-//                context("success") {
-//                    beforeEach {
-//                        apiResult = nil // use default success
-//                    }
-//
-//                    justBeforeEach {
-//                        _ = try! QuickSpec.current.wait(for: recorder.next(), timeout: 5.0)
-//                        continents = try! QuickSpec.current.wait(for: continentsRecorder.next(), timeout: 5.0)
-//                        completion = try! QuickSpec.current.wait(for: recorder.completion, timeout: 5.0)
-//                    }
-//
-//                    it("then value is set") {
-//                        expect(continents.count).to(equal(5))
-//                    }
-//
-//                    it("then recorder has completed") {
-//                        switch completion {
-//                        case .failure:
-//                            fail("Unexpected state")
-//                        case .finished:
-//                            // ok
-//                            break
-//                        case .none:
-//                            fail("Unexpected state")
-//                        }
-//                    }
-//                }
-//
-//                context("failure") {
-//                    beforeEach {
-//                        apiResult = .failure(.domainError(.forbidden, responseCode: .forbidden))
-//                    }
-//
-//                    justBeforeEach {
-//                        completion = try! QuickSpec.current.wait(for: recorder.completion, timeout: 5.0)
-//                    }
-//
-////                    it("then recorder has failed") {
-////                        switch completion {
-////                        case .failure(let error):
-////                            expect(error).to(equal(TravelAdvisoryApiError.domainError(.forbidden, responseCode: .forbidden)))
-////                        case .finished:
-////                            fail("Unexpected state")
-////                        case .none:
-////                            fail("Unexpected state")
-////                        }
-////                    }
-//                }
-//            }
-//        }
-//    }
-//}
+
+import Quick
+import Nimble
+import Swinject
+import Foundation
+import DomainModels
+import CombineExpectations
+import Interfaces
+import Combine
+import Repositories
+import Mockingbird
+import InterfacesMocks
+
+final class CountryListRepositoryTests: QuickSpec {
+    override func spec() {
+        describe("CountryListRepository") {
+            var container: Container!
+            var repository: CountryListRepository!
+            var api: TravelAdvisoryApiImplementingMock!
+            var continentsRecorder: Recorder<[Continent], Never>!
+            var continents: [Continent]!
+
+            beforeEach {
+                container = Container().injectBusinessLogicRepositories()
+                    .injectInterfaceLocalMocks()
+                    .injectInterfaceRemoteMocks()
+                repository = container.resolve(CountryListRepository.self)!
+                api = (container.resolve(TravelAdvisoryApiImplementing.self)! as! TravelAdvisoryApiImplementingMock)
+
+                continentsRecorder = repository.$continents.record()
+                continents = try! QuickSpec.current.wait(for: continentsRecorder.next(), timeout: 5.0)
+            }
+
+            it("then continents starts empty") {
+                expect(continents.count).to(equal(0))
+            }
+
+            describe("#reload") {
+                var recorder: Recorder<Void, CountryListError>!
+                var completion: Subscribers.Completion<CountryListError>!
+
+                context("success") {
+               
+                    justBeforeEach {
+                        _ = try! QuickSpec.current.wait(for: recorder.next(), timeout: 5.0)
+                        continents = try! QuickSpec.current.wait(for: continentsRecorder.next(), timeout: 5.0)
+                        completion = try! QuickSpec.current.wait(for: recorder.completion, timeout: 5.0)
+                    }
+
+                    beforeEach {
+                        given(api.getCountryList()).willReturn(Just(.init()).setFailureType(to: TravelAdvisoryApiError.self).eraseToAnyPublisher())
+                        recorder = repository.reload().record()
+                    }
+                    
+                    it("then value is set") {
+                        expect(continents.count).to(equal(5))
+                    }
+
+                    it("then recorder has completed") {
+                        switch completion {
+                        case .failure:
+                            fail("Unexpected state")
+                        case .finished:
+                            // ok
+                            break
+                        case .none:
+                            fail("Unexpected state")
+                        }
+                    }
+                }
+
+                context("failure") {
+    
+                    beforeEach {
+                        given(api.getCountryList()).willReturn(Fail(error: TravelAdvisoryApiError(statusCode: 10)).eraseToAnyPublisher())
+                        recorder = repository.reload().record()
+                    }
+
+                    justBeforeEach {
+                        completion = try! QuickSpec.current.wait(for: recorder.completion, timeout: 5.0)
+                    }
+
+                    it("then recorder has failed") {
+                        switch completion {
+                        case .failure:
+                            return
+                        case .finished:
+                            fail("Unexpected state")
+                        case .none:
+                            fail("Unexpected state")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
